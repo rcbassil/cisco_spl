fd6196321fb48747036a2290f6338fd5123a4528
Rodrigo Bassil <rodrigo.bassil@sap.com>
2022-05-21 17:21:39 -0400

    Adding userdata.sh, amis, internet gateway and route table.

--- variables.tf
+++ variables.tf
@@ -8,9 +8,10 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
+# ubuntu-server-22.04 64-bit x86
 variable "aws_amis" {
   default = {
-    "us-east-1" = ""
-    "us-west-2" = ""
+    "us-east-1" = "ami-09d56f8956ab235b3"
+    "us-west-2" = "ami-0ee8244746ec5d6d4"
   }
 }
