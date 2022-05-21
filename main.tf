8e75a335d54245e0effe29f3ab74f62508c32474
Rodrigo Bassil <rodrigo.bassil@sap.com>
2022-05-20 23:18:31 -0400

    Adding aws subnet.

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ resource "aws_vpc" "default" {
     Name = "cisco_test"
   }
 }
+
+resource "aws_subnet" "cisco_test_subnet" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "cisco_test_subnet"
+  }
+}
