97bf0a7729282b076075d0d4b0f9f551ca4285ac
Rodrigo Bassil <rodrigo.bassil@sap.com>
2022-05-21 17:38:21 -0400

    Adding outputs.tf with the elb dns, ec2 instance, elb and security group in main.tf

--- main.tf
+++ main.tf
@@ -43,3 +43,129 @@ resource "aws_route_table" "r" {
     Name = "aws_route_table"
   }
 }
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.cisco_test_subnet.id}"
+  route_table_id = "${aws_route_table.r.id}"
+}
+
+# Default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "instance_sg"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Elb security group to access
+# the ELB over HTTP
+resource "aws_security_group" "elb" {
+  name        = "elb_sg"
+  description = "Used in the terraform"
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ensure the VPC has an Internet gateway or this step will fail
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_elb" "web" {
+  name = "cisco-test-elb"
+
+  # The same availability zone as our instance
+  subnets = ["${aws_subnet.cisco_test_subnet.id}"]
+
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+
+  instances                   = ["${aws_instance.web.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+resource "aws_lb_cookie_stickiness_policy" "default" {
+  name                     = "lbpolicy"
+  load_balancer            = "${aws_elb.web.id}"
+  lb_port                  = 80
+  cookie_expiration_period = 600
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  key_name = "${var.key_name}"
+
+  # Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id              = "${aws_subnet.cisco_test_subnet.id}"
+  user_data              = "${file("userdata.sh")}"
+
+  #Instance tags
+
+  tags = {
+    Name = "elb-cisco-test"
+  }
+}
+
